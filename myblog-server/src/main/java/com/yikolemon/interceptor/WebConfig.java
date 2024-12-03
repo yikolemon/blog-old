4a12923c0ea8525f99270f236e5d71f729189e57
duanfuqiang <duanfuqiang@eccom.com.cn>
2024-12-03 17:58:12 +0800

    edit:uv,dau单机内存实现改造

--- myblog-server/src/main/java/com/yikolemon/interceptor/WebConfig.java
+++ myblog-server/src/main/java/com/yikolemon/interceptor/WebConfig.java
@@ -1,27 +1,28 @@
 package com.yikolemon.interceptor;
 
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.context.annotation.Bean;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-//    @Bean
-//    public DataInterceptor dataInterceptor(){
-//        return new DataInterceptor();
-//    }
-//
-//    @Resource
-//    private DataInterceptor dataInterceptor;
-//
-//    @Override
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(dataInterceptor)
-//                .addPathPatterns("/");
-//    }
+    @Bean
+    public DataInterceptor dataInterceptor(){
+        return new DataInterceptor();
+    }
+
+    @Resource
+    private DataInterceptor dataInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(dataInterceptor)
+                .addPathPatterns("/");
+    }
 
 }
